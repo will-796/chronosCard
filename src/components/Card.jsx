8838027fcfd9073387f31495880eedde13af8eb6
willian portela <willianportela1@gmail.com>
2022-06-24 15:08:43 -0300

    req 3

---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+    return (
+      <div>
+        <p data-testid="name-card">{cardName}</p>
+        <p data-testid="description-card">{cardDescription}</p>
+        <p data-testid="attr1-card">{cardAttr1}</p>
+        <p data-testid="attr2-card">{cardAttr2}</p>
+        <p data-testid="attr3-card">{cardAttr3}</p>
+        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+        <p data-testid="rare-card">{cardRare}</p>
+
+        {cardTrunfo && (<p data-testid="trunfo-card">Super Trunfo</p>)}
+      </div>
+    );
+  }
+}
